a7f62cfed07384cd907cec765f41c58fc9c145ec
joe.walnes <joe.walnes@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2007-05-21 22:03:13 +0000

    TypeSafeMatcher.matchesSafely() is now public.

--- hamcrest-library/src/main/java/org/hamcrest/TypeSafeMatcher.java
+++ hamcrest-library/src/main/java/org/hamcrest/TypeSafeMatcher.java
@@ -16,7 +16,7 @@ public abstract class TypeSafeMatcher<T> extends BaseMatcher<T> {
      * Subclasses should implement this. The item will already have been checked for
      * the specific type and will never be null.
      */
-    protected abstract boolean matchesSafely(T item);
+    public abstract boolean matchesSafely(T item);
 
     protected TypeSafeMatcher() {
         expectedType = findExpectedType(getClass());
