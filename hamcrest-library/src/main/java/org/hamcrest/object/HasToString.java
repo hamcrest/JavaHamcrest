850420fbace3d10762cac0d85638d9c86814ba6e
neildunn <neildunn@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2008-06-09 14:53:36 +0000

    Added hasToString(String)

--- hamcrest-library/src/main/java/org/hamcrest/object/HasToString.java
+++ hamcrest-library/src/main/java/org/hamcrest/object/HasToString.java
@@ -1,9 +1,11 @@
 package org.hamcrest.object;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.Factory;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
 
 public class HasToString<T> extends BaseMatcher<T> {
 
@@ -23,9 +25,14 @@ public class HasToString<T> extends BaseMatcher<T> {
 	        .appendDescriptionOf(toStringMatcher)
 	        .appendText(")");
     }
-    
+
     @Factory
     public static <T> Matcher<T> hasToString(Matcher<String> toStringMatcher) {
         return new HasToString<T>(toStringMatcher);
+    }
+
+    @Factory
+    public static <T> Matcher<T> hasToString(String expectedToString) {
+        return new HasToString<T>(equalTo(expectedToString));
     }
 }
