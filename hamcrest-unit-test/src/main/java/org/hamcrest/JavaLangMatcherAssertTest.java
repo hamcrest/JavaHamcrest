9ac3e3680de142fbf43ea446dd59dbf8416245c7
nat.pryce <nat.pryce@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2008-10-16 12:05:29 +0000

    Fixed test so it doesn't matter if assertions are enabled or not

--- hamcrest-unit-test/src/main/java/org/hamcrest/JavaLangMatcherAssertTest.java
+++ hamcrest-unit-test/src/main/java/org/hamcrest/JavaLangMatcherAssertTest.java
@@ -5,25 +5,11 @@ import static org.hamcrest.core.StringStartsWith.startsWith;
 import junit.framework.TestCase;
 
 public class JavaLangMatcherAssertTest extends TestCase {
-    static boolean assertsEnabled = false;
-    static {
-      assert (assertsEnabled = true);  // Intentional side-effect!!!
+    public void testWouldCauseFailingAssertion() throws Exception {
+        assertEquals(false, that("Foo", startsWith("Bar")));
     }
-
-    public void testFailingAssertion() throws Exception {
-        boolean thrown = false;
-        try {
-            assert that("Foo", startsWith("Bar"));
-            // Can't fail() here since catch block will catch JUnit exception.
-        } catch (AssertionError expected) {
-            thrown = true;
-        }
-        if (assertsEnabled && ! thrown) {
-            fail();
-        }
-    }
-
-    public void testPassingAssertion() throws Exception {
-        assert that("Foo", startsWith("F"));
+    
+    public void testWouldCausePassingAssertion() throws Exception {
+        assertEquals(true, that("Foo", startsWith("F")));
     }
 }
