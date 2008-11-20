65c7cc65258ef55828a8ec0223214546275c9f86
smgfreeman <smgfreeman@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2008-11-20 19:33:55 +0000

    sf. To prove Issue 43: Compile errors on hasSize

--- hamcrest-unit-test/src/main/java/org/hamcrest/collection/IsCollectionWithSizeTest.java
+++ hamcrest-unit-test/src/main/java/org/hamcrest/collection/IsCollectionWithSizeTest.java
@@ -1,6 +1,8 @@
 package org.hamcrest.collection;
 
 import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Collection;
 
@@ -9,6 +11,7 @@ import static org.hamcrest.core.IsEqual.equalTo;
 
 import org.hamcrest.AbstractMatcherTest;
 import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
 
 public class IsCollectionWithSizeTest extends AbstractMatcherTest {
 
@@ -65,5 +68,11 @@ public class IsCollectionWithSizeTest extends AbstractMatcherTest {
 
     public void testHasAReadableDescription() {
         assertDescription("a collection with size <3>", hasSize(equalTo(3)));
+    }
+    
+    public void testCompilesWithATypedCollection() {
+      // To prove Issue 43
+      ArrayList<String> arrayList = new ArrayList<String>();
+      MatcherAssert.assertThat(arrayList, hasSize(0));
     }
 }
