db2959c884a288e79e882bc8e7b39caba660556d
smgfreeman <smgfreeman@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2009-03-03 09:36:38 +0000

    restructured test to show descriptions

--- hamcrest-unit-test/src/main/java/org/hamcrest/core/IsNotTest.java
+++ hamcrest-unit-test/src/main/java/org/hamcrest/core/IsNotTest.java
@@ -1,7 +1,8 @@
-/*  Copyright (c) 2000-2006 hamcrest.org
+/*  Copyright (c) 2000-2009 hamcrest.org
  */
 package org.hamcrest.core;
 
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.core.IsNot.not;
 
@@ -9,21 +10,25 @@ import org.hamcrest.AbstractMatcherTest;
 import org.hamcrest.Matcher;
 
 public class IsNotTest extends AbstractMatcherTest {
-    @Override
-    protected Matcher<?> createMatcher() {
-        return not("something");
-    }
+  @Override
+  protected Matcher<?> createMatcher() {
+    return not("something");
+  }
 
-    public void testEvaluatesToTheTheLogicalNegationOfAnotherMatcher() {
-        assertMatches("should match", not(equalTo("A")), "B");
-        assertDoesNotMatch("should not match", not(equalTo("B")), "B");
-    }
+  public void testEvaluatesToTheTheLogicalNegationOfAnotherMatcher() {
+    assertMatches("should match", not(equalTo("A")), "B");
+    assertDoesNotMatch("should not match", not(equalTo("B")), "B");
+  }
 
-    public void testProvidesConvenientShortcutForNotEqualTo() {
-        assertMatches("should match", not("A"), "B");
-        assertMatches("should match", not("B"), "A");
-        assertDoesNotMatch("should not match", not("A"), "A");
-        assertDoesNotMatch("should not match", not("B"), "B");
-        assertDescription("not \"A\"", not("A"));
-    }
-}
+  public void testProvidesConvenientShortcutForNotEqualTo() {
+    assertMatches("should match", not("A"), "B");
+    assertMatches("should match", not("B"), "A");
+    assertDoesNotMatch("should not match", not("A"), "A");
+    assertDoesNotMatch("should not match", not("B"), "B");
+  }
+
+  public void testUsesDescriptionOfNegatedMatcherWithPrefix() {
+    assertDescription("not a value greater than <2>", not(greaterThan(2)));
+    assertDescription("not \"A\"", not("A"));
+  }
+ }
