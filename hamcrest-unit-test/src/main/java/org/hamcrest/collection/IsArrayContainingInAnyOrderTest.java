b3837b0476d695388b672c20090c81b51021cb32
smgfreeman <smgfreeman@2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2>
2009-03-21 11:29:08 +0000

    cleaned up IsArrayContainingInAnyOrder

--- hamcrest-unit-test/src/main/java/org/hamcrest/collection/IsArrayContainingInAnyOrderTest.java
+++ hamcrest-unit-test/src/main/java/org/hamcrest/collection/IsArrayContainingInAnyOrderTest.java
@@ -8,13 +8,38 @@ import org.hamcrest.Matcher;
 
 public class IsArrayContainingInAnyOrderTest extends AbstractMatcherTest {
 
+    @SuppressWarnings("unchecked")
     @Override
     protected Matcher<?> createMatcher() {
         return arrayContainingInAnyOrder(equalTo(1), equalTo(2));
     }
 
+    @SuppressWarnings("unchecked")
     public void testHasAReadableDescription() {
-        assertDescription("[<<1>>, <<2>>] in any order",
-                            arrayContainingInAnyOrder(equalTo(1), equalTo(2)));
+        assertDescription("[<1>, <2>] in any order", arrayContainingInAnyOrder(equalTo(1), equalTo(2)));
+        assertDescription("[<1>, <2>] in any order", arrayContainingInAnyOrder(1, 2));
     }
+    
+    public void testMatchesItemsInAnyOrder() {
+      assertMatches("in order", arrayContainingInAnyOrder(1, 2, 3), new Integer[] {1, 2, 3});
+      assertMatches("out of order", arrayContainingInAnyOrder(1, 2, 3), new Integer[] {3, 2, 1});
+      assertMatches("single", arrayContainingInAnyOrder(1), new Integer[] {1});
+    }
+
+    @SuppressWarnings("unchecked")
+    public void testAppliesMatchersInAnyOrder() {
+      assertMatches("in order", arrayContainingInAnyOrder(equalTo(1), equalTo(2), equalTo(3)), new Integer[] {1, 2, 3});
+      assertMatches("out of order", arrayContainingInAnyOrder(equalTo(1), equalTo(2), equalTo(3)), new Integer[] {3, 2, 1});
+      assertMatches("single", arrayContainingInAnyOrder(equalTo(1)), new Integer[] {1});
+    }
+
+    public void testMismatchesItemsInAnyOrder() {
+      Matcher<Integer[]> matcher = arrayContainingInAnyOrder(1, 2, 3);
+      assertMismatchDescription("was null", matcher, null);
+      assertMismatchDescription("No item matches: <1>, <2>, <3> in []", matcher, new Integer[] {});
+      assertMismatchDescription("No item matches: <2>, <3> in [<1>]", matcher, new Integer[] {1});
+      assertMismatchDescription("Not matched: <4>", matcher, new Integer[] {4,3,2,1});
+    }
+
+    
 }
