1ab1597b1abe0d12663ddcadd55511281b6ba4d7
sf105 <github@m3p.co.uk>
2012-04-29 12:22:28 +0100

    CombinableMatcher now self diagnosing (issue 179)

--- hamcrest-unit-test/src/main/java/org/hamcrest/AbstractMatcherTest.java
+++ hamcrest-unit-test/src/main/java/org/hamcrest/AbstractMatcherTest.java
@@ -3,6 +3,7 @@
 package org.hamcrest;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
 
 public abstract class AbstractMatcherTest extends TestCase {
 
@@ -12,24 +13,24 @@ public abstract class AbstractMatcherTest extends TestCase {
     protected abstract Matcher<?> createMatcher();
     
     public static <T> void assertMatches(String message, Matcher<? super T> c, T arg) {
-        assertTrue(message, c.matches(arg));
+        Assert.assertTrue(message, c.matches(arg));
     }
 
     public static <T> void assertDoesNotMatch(String message, Matcher<? super T> c, T arg) {
-        assertFalse(message, c.matches(arg));
+        Assert.assertFalse(message, c.matches(arg));
     }
 
     public static void assertDescription(String expected, Matcher<?> matcher) {
         Description description = new StringDescription();
         description.appendDescriptionOf(matcher);
-        assertEquals("Expected description", expected, description.toString().trim());
+        Assert.assertEquals("Expected description", expected, description.toString().trim());
     }
 
     public static <T> void assertMismatchDescription(String expected, Matcher<? super T> matcher, T arg) {
-        assertFalse("Precondtion: Matcher should not match item.", matcher.matches(arg));
+        Assert.assertFalse("Precondtion: Matcher should not match item.", matcher.matches(arg));
         Description description = new StringDescription();
         matcher.describeMismatch(arg, description);
-        assertEquals("Expected mismatch description", expected, description.toString().trim());
+        Assert.assertEquals("Expected mismatch description", expected, description.toString().trim());
     }
 
     public void testIsNullSafe() {
--- hamcrest-unit-test/src/main/java/org/hamcrest/core/CombinableTest.java
+++ hamcrest-unit-test/src/main/java/org/hamcrest/core/CombinableTest.java
@@ -1,9 +1,9 @@
 package org.hamcrest.core;
 
-import org.hamcrest.StringDescription;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.AbstractMatcherTest.assertMismatchDescription;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.CombinableMatcher.both;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -13,56 +13,50 @@ import static org.hamcrest.number.OrderingComparison.greaterThan;
 import static org.junit.Assert.assertEquals;
 
 public class CombinableTest {
-    private static final CombinableMatcher<Integer> EITHER_3_OR_4 = CombinableMatcher.<Integer>either(equalTo(3)).or(equalTo(4));
-    private static final CombinableMatcher<Integer> NOT_3_AND_NOT_4 = CombinableMatcher.<Integer>both(not(equalTo(3))).and(not(equalTo(4)));
+  private static final CombinableMatcher<Integer> EITHER_3_OR_4 = CombinableMatcher.<Integer>either(equalTo(3)).or(equalTo(4));
+  private static final CombinableMatcher<Integer> NOT_3_AND_NOT_4 = CombinableMatcher.<Integer>both(not(equalTo(3))).and(not(equalTo(4)));
 
-    @Test
-    public void bothAcceptsAndRejects() {
-        assertThat(2, NOT_3_AND_NOT_4);
-        assertThat(3, not(NOT_3_AND_NOT_4));
-    }
+  @Test
+  public void bothAcceptsAndRejects() {
+    assertThat(2, NOT_3_AND_NOT_4);
+    assertThat(3, not(NOT_3_AND_NOT_4));
+  }
 
-    @Test
-    public void acceptsAndRejectsThreeAnds() {
-        CombinableMatcher<? super Integer> tripleAnd = NOT_3_AND_NOT_4.and(equalTo(2));
-        assertThat(2, tripleAnd);
-        assertThat(3, not(tripleAnd));
-    }
+  @Test
+  public void acceptsAndRejectsThreeAnds() {
+    CombinableMatcher<? super Integer> tripleAnd = NOT_3_AND_NOT_4.and(equalTo(2));
+    assertThat(2, tripleAnd);
+    assertThat(3, not(tripleAnd));
+  }
 
 
-    @Test
-    public void bothDescribesItself() {
-        assertEquals("(not <3> and not <4>)", NOT_3_AND_NOT_4.toString());
+  @Test
+  public void bothDescribesItself() {
+    assertEquals("(not <3> and not <4>)", NOT_3_AND_NOT_4.toString());
+    assertMismatchDescription("not <3> was <3>", NOT_3_AND_NOT_4, 3);
+  }
 
-        StringDescription mismatch = new StringDescription();
-        NOT_3_AND_NOT_4.describeMismatch(3, mismatch);
-        assertEquals("was <3>", mismatch.toString());
-    }
+  @Test
+  public void eitherAcceptsAndRejects() {
+    assertThat(3, EITHER_3_OR_4);
+    assertThat(6, not(EITHER_3_OR_4));
+  }
 
-    @Test
-    public void eitherAcceptsAndRejects() {
-        assertThat(3, EITHER_3_OR_4);
-        assertThat(6, not(EITHER_3_OR_4));
-    }
+  @Test
+  public void acceptsAndRejectsThreeOrs() {
+    final CombinableMatcher<Integer> orTriple = EITHER_3_OR_4.or(greaterThan(10));
+    assertThat(11, orTriple);
+    assertThat(9, not(orTriple));
+  }
 
-    @Test
-    public void acceptsAndRejectsThreeOrs() {
-        final CombinableMatcher<Integer> orTriple = EITHER_3_OR_4.or(greaterThan(10));
-        assertThat(11, orTriple);
-        assertThat(9, not(orTriple));
-    }
+  @Test
+  public void eitherDescribesItself() {
+    Assert.assertEquals("(<3> or <4>)", EITHER_3_OR_4.toString());
+    assertMismatchDescription("was <6>", EITHER_3_OR_4, 6);
+  }
 
-    @Test
-    public void eitherDescribesItself() {
-        Assert.assertEquals("(<3> or <4>)", EITHER_3_OR_4.toString());
-        StringDescription mismatch = new StringDescription();
-
-        EITHER_3_OR_4.describeMismatch(6, mismatch);
-        Assert.assertEquals("was <6>", mismatch.toString());
-    }
-
-    @Test
-    public void picksUpTypeFromLeftHandSideOfExpression() {
-        assertThat("yellow", both(equalTo("yellow")).and(notNullValue()));
-    }
+  @Test
+  public void picksUpTypeFromLeftHandSideOfExpression() {
+    assertThat("yellow", both(equalTo("yellow")).and(notNullValue()));
+  }
 }
