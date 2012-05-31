20e8b9ccca186f384c2cd239ce205ac0c6802d45
scarytom <t.denley@cantab.net>
2012-05-31 20:52:55 +0100

    add unit tests for BaseDescription, in preparation for attacking issue 184

---
+++ hamcrest-unit-test/src/main/java/org/hamcrest/BaseDescriptionTest.java
@@ -0,0 +1,55 @@
+package org.hamcrest;
+
+import junit.framework.TestCase;
+
+public class BaseDescriptionTest extends TestCase {
+
+    private final StringBuilder result = new StringBuilder();
+
+    private final BaseDescription baseDescription = new BaseDescription() {
+        @Override protected void append(char c) {
+            result.append(c);
+        }
+    };
+
+    public void testDescribesAppendedNullValue() {
+        baseDescription.appendValue(null);
+        assertEquals("null", result.toString());
+    }
+
+    public void testQuotesAppendedStringValue() {
+        baseDescription.appendValue("foo");
+        assertEquals("\"foo\"", result.toString());
+    }
+
+    public void testQuotesAppendedCharacterValue() {
+        baseDescription.appendValue('f');
+        assertEquals("\"f\"", result.toString());
+    }
+
+    public void testBracketsAppendedShortValue() {
+        baseDescription.appendValue(Short.valueOf("2"));
+        assertEquals("<2s>", result.toString());
+    }
+
+    public void testBracketsAppendedLongValue() {
+        baseDescription.appendValue(Long.valueOf("2"));
+        assertEquals("<2L>", result.toString());
+    }
+
+    public void testBracketsAppendedFloatValue() {
+        baseDescription.appendValue(Float.valueOf("1.2"));
+        assertEquals("<1.2F>", result.toString());
+    }
+
+    public void testDescribesAppendedArrayValue() {
+        baseDescription.appendValue(new String[] {"2", "3"});
+        assertEquals("[\"2\", \"3\"]", result.toString());
+    }
+
+    public void testBracketsAppendedObjectValue() {
+        final Object value = new Object();
+        baseDescription.appendValue(value);
+        assertEquals("<" + value.toString() + ">", result.toString());
+    }
+}
