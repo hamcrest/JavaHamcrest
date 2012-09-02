55d8dccdb7b6eecacfe2f2731de04d14dac0de6e
sf105 <github@m3p.co.uk>
2012-09-02 21:38:37 +0100

    added test for descriptions

--- hamcrest-unit-test/src/main/java/org/hamcrest/number/BigDecimalCloseToTest.java
+++ hamcrest-unit-test/src/main/java/org/hamcrest/number/BigDecimalCloseToTest.java
@@ -8,6 +8,7 @@ import java.math.BigDecimal;
 import static org.hamcrest.number.BigDecimalCloseTo.closeTo;
 
 public class BigDecimalCloseToTest  extends AbstractMatcherTest {
+  private final Matcher<BigDecimal> matcher = closeTo(new BigDecimal("1.0"), new BigDecimal("0.5"));
 
   @Override
   protected Matcher<?> createMatcher() {
@@ -16,29 +17,29 @@ public class BigDecimalCloseToTest  extends AbstractMatcherTest {
   }
   
   public void testEvaluatesToTrueIfArgumentIsEqualToABigDecimalWithinSomeError() {
-    Matcher<BigDecimal> p = closeTo(new BigDecimal("1.0"), new BigDecimal("0.5"));
+    assertTrue(matcher.matches(new BigDecimal("1.0")));
+    assertTrue(matcher.matches(new BigDecimal("0.5")));
+    assertTrue(matcher.matches(new BigDecimal("1.5")));
 
-    assertTrue(p.matches(new BigDecimal("1.0")));
-    assertTrue(p.matches(new BigDecimal("0.5")));
-    assertTrue(p.matches(new BigDecimal("1.5")));
-
-    assertDoesNotMatch("too large", p, new BigDecimal("2.0"));
-    assertMismatchDescription("<2.0> differed by <0.5> more than delta <0.5>", p, new BigDecimal("2.0"));
-    assertDoesNotMatch("number too small", p, new BigDecimal("0.0"));
-    assertMismatchDescription("<0.0> differed by <0.5> more than delta <0.5>", p, new BigDecimal("0.0"));
+    assertDoesNotMatch("too large", matcher, new BigDecimal("2.0"));
+    assertMismatchDescription("<2.0> differed by <0.5> more than delta <0.5>", matcher, new BigDecimal("2.0"));
+    assertDoesNotMatch("number too small", matcher, new BigDecimal("0.0"));
+    assertMismatchDescription("<0.0> differed by <0.5> more than delta <0.5>", matcher, new BigDecimal("0.0"));
   }
   
   public void testEvaluatesToTrueIfArgumentHasDifferentScale() {
-    Matcher<BigDecimal> p = closeTo(new BigDecimal("1.0"), new BigDecimal("0.5"));
+    assertTrue(matcher.matches(new BigDecimal("1.000000")));
+    assertTrue(matcher.matches(new BigDecimal("0.500000")));
+    assertTrue(matcher.matches(new BigDecimal("1.500000")));
 
-    assertTrue(p.matches(new BigDecimal("1.000000")));
-    assertTrue(p.matches(new BigDecimal("0.500000")));
-    assertTrue(p.matches(new BigDecimal("1.500000")));
+    assertDoesNotMatch("too large", matcher, new BigDecimal("2.000000"));
+    assertMismatchDescription("<2.000000> differed by <0.5> more than delta <0.5>", matcher, new BigDecimal("2.000000"));
+    assertDoesNotMatch("number too small", matcher, new BigDecimal("0.000000"));
+    assertMismatchDescription("<0.000000> differed by <0.5> more than delta <0.5>", matcher, new BigDecimal("0.000000"));
+  }
 
-    assertDoesNotMatch("too large", p, new BigDecimal("2.000000"));
-    assertMismatchDescription("<2.000000> differed by <0.5> more than delta <0.5>", p, new BigDecimal("2.000000"));
-    assertDoesNotMatch("number too small", p, new BigDecimal("0.000000"));
-    assertMismatchDescription("<0.000000> differed by <0.5> more than delta <0.5>", p, new BigDecimal("0.000000"));
+  public void test_is_self_describing() {
+    assertDescription("a numeric value within <0.5> of <1.0>", matcher);
   }
 
 }
--- hamcrest-unit-test/src/main/java/org/hamcrest/number/IsCloseToTest.java
+++ hamcrest-unit-test/src/main/java/org/hamcrest/number/IsCloseToTest.java
@@ -8,23 +8,27 @@ import org.hamcrest.Matcher;
 import static org.hamcrest.number.IsCloseTo.closeTo;
 
 public class IsCloseToTest extends AbstractMatcherTest {
+  private final Matcher<Double> matcher = closeTo(1.0d, 0.5d);
 
-    @Override
+  @Override
     protected Matcher<?> createMatcher() {
         final double irrelevant = 0.1;
         return closeTo(irrelevant, irrelevant);
     }
 
     public void test_matchesIfArgumentIsEqualToADoubleValueWithinSomeError() {
-        final Matcher<Double> p = closeTo(1.0d, 0.5d);
+        assertMatches("1.0", matcher, 1.0);
+        assertMatches("0.5d", matcher, 0.5d);
+        assertMatches("1.5d", matcher, 1.5d);
 
-        assertMatches("1.0", p, 1.0);
-        assertMatches("0.5d", p, 0.5d);
-        assertMatches("1.5d", p, 1.5d);
-
-        assertDoesNotMatch("too large", p, 2.0);
-        assertMismatchDescription("<3.0> differed by <1.5> more than delta <0.5>", p, 3.0d);
-        assertDoesNotMatch("number too small", p, 0.0);
-        assertMismatchDescription("<0.1> differed by <0.4> more than delta <0.5>", p, 0.1);
+        assertDoesNotMatch("too large", matcher, 2.0);
+        assertMismatchDescription("<3.0> differed by <1.5> more than delta <0.5>", matcher, 3.0d);
+        assertDoesNotMatch("number too small", matcher, 0.0);
+        assertMismatchDescription("<0.1> differed by <0.4> more than delta <0.5>", matcher, 0.1);
     }
+
+    public void test_is_self_describing() {
+        assertDescription("a numeric value within <0.5> of <1.0>", matcher);
+    }
+
 }
