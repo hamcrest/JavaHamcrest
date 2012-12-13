72520cb8d8d47614cf105c0848fb753f6efbea33
Tom Denley <t.denley@cantab.net>
2012-12-13 09:54:35 +0000

    Update hamcrest-core/src/main/java/org/hamcrest/internal/ReflectiveTypeFinder.java
    typo

--- hamcrest-core/src/main/java/org/hamcrest/internal/ReflectiveTypeFinder.java
+++ hamcrest-core/src/main/java/org/hamcrest/internal/ReflectiveTypeFinder.java
@@ -13,7 +13,7 @@
  * 
  * A <code>FeatureMatcher</code> is an abstract subclass of <code>TypeSafeDiagnosingMatcher</code>. 
  * Although it has a templated implementation of <code>matchesSafely(&lt;T&gt;, Description);</code>, the
- * actually run-time signature of this is <code>matchesSafely(Object, Description);</code>. Instead,
+ * actual run-time signature of this is <code>matchesSafely(Object, Description);</code>. Instead,
  * we must find the type by reflecting on the concrete implementation of 
  * <pre>protected abstract U featureValueOf(T actual);</pre>
  * a method which is declared in <code>FeatureMatcher</code>.
