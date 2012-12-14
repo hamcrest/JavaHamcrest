3b90b9a439f48fb22e9d261f690de524f1a568e7
scarytom <t.denley@cantab.net>
2012-12-14 11:02:35 +0000

    test for api

---
+++ hamcrest-api/src/test/java/org/hamcrest/NullDescriptionTest.java
@@ -0,0 +1,18 @@
+package org.hamcrest;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.Description.NullDescription;
+import org.junit.Test;
+
+public final class NullDescriptionTest {
+
+    private final NullDescription nullDescription = new Description.NullDescription();
+
+    @Test public void
+    isUnchangedByAppendedText() {
+        nullDescription.appendText("myText");
+        assertEquals("", nullDescription.toString());
+    }
+
+}
