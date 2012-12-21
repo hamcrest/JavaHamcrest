898d274104fa5c8230365be993aeac6d53203431
Tom Denley <tom.denley@youdevise.com>
2012-12-21 12:39:30 +0000

    convert to JUnit4

--- hamcrest-core/src/test/java/org/hamcrest/core/IsCollectionContainingTest.java
+++ hamcrest-core/src/test/java/org/hamcrest/core/IsCollectionContainingTest.java
@@ -1,108 +1,112 @@
 package org.hamcrest.core;
 
-import org.hamcrest.AbstractMatcherTest;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeDiagnosingMatcher;
+import static java.util.Arrays.asList;
+import static org.hamcrest.AbstractMatcherTest.assertDescription;
+import static org.hamcrest.AbstractMatcherTest.assertDoesNotMatch;
+import static org.hamcrest.AbstractMatcherTest.assertMatches;
+import static org.hamcrest.AbstractMatcherTest.assertMismatchDescription;
+import static org.hamcrest.AbstractMatcherTest.assertNullSafe;
+import static org.hamcrest.AbstractMatcherTest.assertUnknownTypeSafe;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
-import static java.util.Arrays.asList;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsCollectionContaining.hasItem;
-import static org.hamcrest.core.IsCollectionContaining.hasItems;
-import static org.hamcrest.core.IsEqual.equalTo;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.junit.Test;
 
-public class IsCollectionContainingTest extends AbstractMatcherTest {
-    @Override
-    protected Matcher<?> createMatcher() {
-        return hasItem(equalTo("irrelevant"));
+public final class IsCollectionContainingTest {
+    
+    @Test public void
+    copesWithNullsAndUnknownTypes() {
+        Matcher<?> matcher = hasItem(equalTo("irrelevant"));
+        
+        assertNullSafe(matcher);
+        assertUnknownTypeSafe(matcher);
     }
 
-    public void testMatchesACollectionThatContainsAnElementForTheGivenMatcher() {
+    @Test public void
+    matchesACollectionThatContainsAnElementForTheGivenMatcher() {
         final Matcher<Iterable<? super String>> itemMatcher = hasItem(equalTo("a"));
 
-        assertMatches("list containing 'a'",
-                      itemMatcher, asList("a", "b", "c"));
+        assertMatches("list containing 'a'", itemMatcher, asList("a", "b", "c"));
     }
 
-    public void testDoesNotMatchCollectionWithoutAnElementForGivenMatcher() {
+    @Test public void
+    doesNotMatchCollectionWithoutAnElementForGivenMatcher() {
         final Matcher<Iterable<? super String>> matcher = hasItem(mismatchable("a"));
-        assertMismatchDescription("mismatches were: [mismatched: b, mismatched: c]",
-                                  matcher, asList("b", "c"));
         
+        assertMismatchDescription("mismatches were: [mismatched: b, mismatched: c]", matcher, asList("b", "c"));
         assertMismatchDescription("was empty", matcher, new ArrayList<String>());
     }
 
-    public void testDoesNotMatchNull() {
+    @Test public void
+    doesNotMatchNull() {
         assertDoesNotMatch("doesn't match null", hasItem(equalTo("a")), null);
     }
 
-    public void testHasAReadableDescription() {
+    @Test public void
+    hasAReadableDescription() {
         assertDescription("a collection containing mismatchable: a", hasItem(mismatchable("a")));
     }
     
-    public void testCanMatchItemWhenCollectionHoldsSuperclass() // Issue 24
-    {
-      final Set<Number> s = new HashSet<Number>();
-      s.add(2);
-      assertThat(s, new IsCollectionContaining<Number>(new IsEqual<Number>(2)));
-      assertThat(s, IsCollectionContaining.hasItem(2));
+    @Test public void
+    canMatchItemWhenCollectionHoldsSuperclass() { // Issue 24
+        final Set<Number> s = new HashSet<Number>();
+        s.add(2);
+
+        assertMatches("didn't match", new IsCollectionContaining<Number>(new IsEqual<Number>(2)), s);
+        assertMatches("didn't match", IsCollectionContaining.hasItem(2), s);
     }
 
     @SuppressWarnings("unchecked")
-    public void testMatchesMultipleItemsInCollection() {
+    @Test public void
+    matchesMultipleItemsInCollection() {
         final Matcher<Iterable<String>> matcher1 = hasItems(equalTo("a"), equalTo("b"), equalTo("c"));
-        assertMatches("list containing all items",
-                matcher1,
-                asList("a", "b", "c"));
+        assertMatches("list containing all items", matcher1, asList("a", "b", "c"));
         
         final Matcher<Iterable<String>> matcher2 = hasItems("a", "b", "c");
-        assertMatches("list containing all items (without matchers)",
-                matcher2,
-                asList("a", "b", "c"));
+        assertMatches("list containing all items (without matchers)", matcher2, asList("a", "b", "c"));
         
         final Matcher<Iterable<String>> matcher3 = hasItems(equalTo("a"), equalTo("b"), equalTo("c"));
-        assertMatches("list containing all items in any order",
-                matcher3,
-                asList("c", "b", "a"));
+        assertMatches("list containing all items in any order", matcher3, asList("c", "b", "a"));
         
         final Matcher<Iterable<String>> matcher4 = hasItems(equalTo("a"), equalTo("b"), equalTo("c"));
-        assertMatches("list containing all items plus others",
-                matcher4,
-                asList("e", "c", "b", "a", "d"));
+        assertMatches("list containing all items plus others", matcher4, asList("e", "c", "b", "a", "d"));
         
         final Matcher<Iterable<String>> matcher5 = hasItems(equalTo("a"), equalTo("b"), equalTo("c"));
-        assertDoesNotMatch("not match list unless it contains all items",
-                matcher5,
-                asList("e", "c", "b", "d")); // 'a' missing
+        assertDoesNotMatch("not match list unless it contains all items", matcher5, asList("e", "c", "b", "d")); // 'a' missing
     }
     
-    public void testReportsMismatchWithAReadableDescriptionForMultipleItems() {
+    @Test public void
+    reportsMismatchWithAReadableDescriptionForMultipleItems() {
         final Matcher<Iterable<Integer>> matcher = hasItems(3, 4);
         
         assertMismatchDescription("a collection containing <4> mismatches were: [was <1>, was <2>, was <3>]",
                                   matcher, asList(1, 2, 3));
     }
-    
-    private static Matcher<? super String> mismatchable(final String string) {
-      return new TypeSafeDiagnosingMatcher<String>() {
-        @Override
-        protected boolean matchesSafely(String item, Description mismatchDescription) {
-          if (string.equals(item)) 
-            return true;
-          
-          mismatchDescription.appendText("mismatched: " + item);
-          return false;
-        }
 
-        @Override
-        public void describeTo(Description description) {
-          description.appendText("mismatchable: " + string);
-        }
-      };
+    private static Matcher<? super String> mismatchable(final String string) {
+        return new TypeSafeDiagnosingMatcher<String>() {
+            @Override
+            protected boolean matchesSafely(String item, Description mismatchDescription) {
+                if (string.equals(item)) 
+                    return true;
+
+                mismatchDescription.appendText("mismatched: " + item);
+                return false;
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("mismatchable: " + string);
+            }
+        };
     }
 }
 
