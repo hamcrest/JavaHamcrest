bc7f1f9f05d48841994780fed5b51b6d08f217f2
scarytom <t.denley@cantab.net>
2013-01-26 21:08:19 +0000

    Enhance the IsEmptyString matcher to match blank strings too.

--- hamcrest-library/src/main/java/org/hamcrest/text/IsEmptyString.java
+++ hamcrest-library/src/main/java/org/hamcrest/text/IsEmptyString.java
@@ -4,27 +4,42 @@ package org.hamcrest.text;
 import static org.hamcrest.core.AnyOf.anyOf;
 import static org.hamcrest.core.IsNull.nullValue;
 
+import java.util.regex.Pattern;
+
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.hamcrest.Factory;
 import org.hamcrest.Matcher;
 
 /**
- * Matches empty Strings (and null).
+ * Provides matchers that match null, empty, or blank Strings, or combinations thereof.
  */
 public final class IsEmptyString extends BaseMatcher<String> {
-    private static final IsEmptyString INSTANCE = new IsEmptyString();
+    private static final IsEmptyString EMPTY_INSTANCE = new IsEmptyString(false);
+    private static final IsEmptyString BLANK_INSTANCE = new IsEmptyString(true);
     @SuppressWarnings("unchecked")
-    private static final Matcher<String> NULL_OR_EMPTY_INSTANCE = anyOf(nullValue(), INSTANCE);
+    private static final Matcher<String> NULL_OR_EMPTY_INSTANCE = anyOf(nullValue(), EMPTY_INSTANCE);
+
+    private static final Pattern REGEX_WHITESPACE = Pattern.compile("\\s*");
+
+    private final boolean allowWhitespace;
+
+    private IsEmptyString(boolean allowWhitespace) {
+        this.allowWhitespace = allowWhitespace;
+    }
 
     @Override
     public boolean matches(Object item) {
-        return item != null && item instanceof String && ((String) item).equals("");
+        if (item == null || !(item instanceof String)) {
+            return false;
+        }
+        final String candidate = (String)item;
+        return allowWhitespace ? REGEX_WHITESPACE.matcher(candidate).matches() : candidate.equals("");
     }
 
     @Override
     public void describeTo(Description description) {
-        description.appendText("an empty string");
+        description.appendText(allowWhitespace ? "a blank" : "an empty").appendText(" string");
     }
 
     /**
@@ -40,7 +55,7 @@ public final class IsEmptyString extends BaseMatcher<String> {
     public static Matcher<String> isEmptyString() {
         return emptyString();
     }
-    
+
     /**
      * Creates a matcher of {@link String} that matches when the examined string has zero length.
      * <p/>
@@ -50,7 +65,19 @@ public final class IsEmptyString extends BaseMatcher<String> {
      */
     @Factory
     public static Matcher<String> emptyString() {
-        return INSTANCE;
+        return EMPTY_INSTANCE;
+    }
+
+    /**
+     * Creates a matcher of {@link String} that matches when the examined string contains
+     * zero or more whitespace characters and nothing else.
+     * <p/>
+     * For example:
+     * <pre>assertThat("  ", is(emptyString()))</pre>
+     */
+    @Factory
+    public static Matcher<String> blankString() {
+        return BLANK_INSTANCE;
     }
 
     /**
@@ -68,7 +95,7 @@ public final class IsEmptyString extends BaseMatcher<String> {
     public static Matcher<String> isEmptyOrNullString() {
         return emptyOrNullString();
     }
-    
+
     /**
      * Creates a matcher of {@link String} that matches when the examined string is <code>null</code>, or
      * has zero length.
--- hamcrest-library/src/test/java/org/hamcrest/text/IsEmptyStringTest.java
+++ hamcrest-library/src/test/java/org/hamcrest/text/IsEmptyStringTest.java
@@ -8,6 +8,7 @@ import static org.hamcrest.AbstractMatcherTest.assertNullSafe;
 import static org.hamcrest.AbstractMatcherTest.assertUnknownTypeSafe;
 import static org.hamcrest.text.IsEmptyString.emptyOrNullString;
 import static org.hamcrest.text.IsEmptyString.emptyString;
+import static org.hamcrest.text.IsEmptyString.blankString;
 
 import org.hamcrest.Matcher;
 import org.junit.Test;
@@ -23,32 +24,44 @@ public final class IsEmptyStringTest {
     }
 
     @Test public void
-    matchesNullAppropriately() {
-        assertMatches("didn't match", emptyOrNullString(), null);
-        assertDoesNotMatch("matched unexpectedly", emptyString(), null);
-    }
-
-    @Test public void
     matchesEmptyString() {
         assertMatches("didn't match", emptyOrNullString(), "");
         assertMatches("didn't match", emptyString(), "");
+        assertMatches("didn't match", blankString(), "");
+    }
+
+    @Test public void
+    matchesNullAppropriately() {
+        assertMatches("didn't match", emptyOrNullString(), null);
+        assertDoesNotMatch("matched unexpectedly", emptyString(), null);
+        assertDoesNotMatch("matched unexpectedly", blankString(), null);
+    }
+
+    @Test public void
+    matchesBlankStringAppropriately() {
+        assertMatches("didn't match", blankString(), "  ");
+        assertDoesNotMatch("matched unexpectedly", emptyString(), "  ");
+        assertDoesNotMatch("matched unexpectedly", emptyOrNullString(), "  ");
     }
 
     @Test public void
     doesNotmatchFilledString() {
-        assertDoesNotMatch("matched unexpectedly", emptyOrNullString(), "a");
         assertDoesNotMatch("matched unexpectedly", emptyString(), "a");
+        assertDoesNotMatch("matched unexpectedly", blankString(), "a");
+        assertDoesNotMatch("matched unexpectedly", emptyOrNullString(), "a");
     }
 
     @Test public void
     describesItself() {
         assertDescription("an empty string", emptyString());
+        assertDescription("a blank string", blankString());
         assertDescription("(null or an empty string)", emptyOrNullString());
     }
 
     @Test public void
     describesAMismatch() {
         assertMismatchDescription("was \"a\"", emptyString(), "a");
+        assertMismatchDescription("was \"a\"", blankString(), "a");
         assertMismatchDescription("was \"a\"", emptyOrNullString(), "a");
     }
 }
