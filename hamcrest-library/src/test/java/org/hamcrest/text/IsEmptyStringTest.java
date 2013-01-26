a385b9317207f47be934e01fbeee4b88df3e7160
scarytom <t.denley@cantab.net>
2013-01-26 19:00:28 +0000

    Convert test to JUnit4 (and add mismatch description tests) in preparation for enhancement.

--- hamcrest-library/src/test/java/org/hamcrest/text/IsEmptyStringTest.java
+++ hamcrest-library/src/test/java/org/hamcrest/text/IsEmptyStringTest.java
@@ -1,40 +1,54 @@
 package org.hamcrest.text;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.AbstractMatcherTest.assertDescription;
+import static org.hamcrest.AbstractMatcherTest.assertDoesNotMatch;
+import static org.hamcrest.AbstractMatcherTest.assertMatches;
+import static org.hamcrest.AbstractMatcherTest.assertMismatchDescription;
+import static org.hamcrest.AbstractMatcherTest.assertNullSafe;
+import static org.hamcrest.AbstractMatcherTest.assertUnknownTypeSafe;
 import static org.hamcrest.text.IsEmptyString.emptyOrNullString;
 import static org.hamcrest.text.IsEmptyString.emptyString;
 
-import org.hamcrest.AbstractMatcherTest;
 import org.hamcrest.Matcher;
+import org.junit.Test;
 
-public class IsEmptyStringTest extends AbstractMatcherTest {
+public final class IsEmptyStringTest {
 
-  @Override
-  protected Matcher<?> createMatcher() {
-    return emptyOrNullString();
-  }
+    @Test public void
+    copesWithNullsAndUnknownTypes() {
+        Matcher<String> matcher = emptyOrNullString();
+        
+        assertNullSafe(matcher);
+        assertUnknownTypeSafe(matcher);
+    }
 
-  public void testEmptyOrNullIsNull() {
-    assertThat(null, emptyOrNullString());
-  }
+    @Test public void
+    matchesNullAppropriately() {
+        assertMatches("didn't match", emptyOrNullString(), null);
+        assertDoesNotMatch("matched unexpectedly", emptyString(), null);
+    }
 
+    @Test public void
+    matchesEmptyString() {
+        assertMatches("didn't match", emptyOrNullString(), "");
+        assertMatches("didn't match", emptyString(), "");
+    }
 
-  public void testEmptyIsNotNull() {
-    assertThat(null, not(emptyString()));
-  }
+    @Test public void
+    doesNotmatchFilledString() {
+        assertDoesNotMatch("matched unexpectedly", emptyOrNullString(), "a");
+        assertDoesNotMatch("matched unexpectedly", emptyString(), "a");
+    }
 
-  public void testMatchesEmptyString() {
-    assertMatches("empty string", emptyString(), "");
-    assertMatches("empty string", emptyOrNullString(), "");
-  }
+    @Test public void
+    describesItself() {
+        assertDescription("an empty string", emptyString());
+        assertDescription("(null or an empty string)", emptyOrNullString());
+    }
 
-  public void testDoesNotMatchNonEmptyString() {
-    assertDoesNotMatch("non empty string", emptyString(), "a");
-  }
-
-  public void testHasAReadableDescription() {
-    assertDescription("an empty string", emptyString());
-    assertDescription("(null or an empty string)", emptyOrNullString());
-  }
+    @Test public void
+    describesAMismatch() {
+        assertMismatchDescription("was \"a\"", emptyString(), "a");
+        assertMismatchDescription("was \"a\"", emptyOrNullString(), "a");
+    }
 }
