f39c1e6a7f971436c1cca52986f3e09fe6eaabfb
scarytom <t.denley@cantab.net>
2013-01-27 10:39:22 +0000

    Introduce Double Not-a-number (NaN) matcher to help resolve GitHub issue #7

---
+++ hamcrest-library/src/test/java/org/hamcrest/number/IsNanTest.java
@@ -0,0 +1,48 @@
+package org.hamcrest.number;
+
+import static org.hamcrest.AbstractMatcherTest.assertDescription;
+import static org.hamcrest.AbstractMatcherTest.assertDoesNotMatch;
+import static org.hamcrest.AbstractMatcherTest.assertMatches;
+import static org.hamcrest.AbstractMatcherTest.assertMismatchDescription;
+import static org.hamcrest.AbstractMatcherTest.assertNullSafe;
+import static org.hamcrest.AbstractMatcherTest.assertUnknownTypeSafe;
+import static org.hamcrest.number.IsNaN.notANumber;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+
+public final class IsNanTest {
+
+    @Test public void
+    copesWithNullsAndUnknownTypes() {
+        Matcher<Double> matcher = notANumber();
+        
+        assertNullSafe(matcher);
+        assertUnknownTypeSafe(matcher);
+    }
+
+    @Test public void
+    matchesNaN() {
+        assertMatches("didn't match", notANumber(), Double.NaN);
+    }
+
+    @Test public void
+    doesNotMatchDoubleValue() {
+        assertDoesNotMatch("matched unexpectedly", notANumber(), 1.25);
+    }
+
+    @Test public void
+    doesNotMatchInfinity() {
+        assertDoesNotMatch("matched unexpectedly", notANumber(), Double.POSITIVE_INFINITY);
+    }
+
+    @Test public void
+    describesItself() {
+        assertDescription("a double value of NaN", notANumber());
+    }
+
+    @Test public void
+    describesAMismatch() {
+        assertMismatchDescription("was <1.25>", notANumber(), 1.25);
+    }
+}
