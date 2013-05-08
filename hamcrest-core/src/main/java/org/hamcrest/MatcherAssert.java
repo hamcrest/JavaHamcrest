930813b7c50c0b98ecca590dbbfc3f62addc2750
Mark Michaelis <mark.michaelis@coremedia.com>
2013-05-08 22:47:09 +0200

    Introduce MatcherExecute as alternative to MatcherAssert.
    Having MatcherAssert projects like JUnit cannot reuse the strategy
    to create mismatch descriptions. Thus for example JUnit's Assume
    creates different failure message than JUnit's Assert.
    
    Having the new MatcherExecute with its FailStrategies you can easily
    add any failure behavior, most notably any exception raised.
    
    Refactored MatcherAssert to use the new MatcherExecute.

--- hamcrest-core/src/main/java/org/hamcrest/MatcherAssert.java
+++ hamcrest-core/src/main/java/org/hamcrest/MatcherAssert.java
@@ -3,24 +3,17 @@
 package org.hamcrest;
 
 
+import static org.hamcrest.MatcherExecute.AssertionErrorStrategy;
+
 public class MatcherAssert {
     public static <T> void assertThat(T actual, Matcher<? super T> matcher) {
         assertThat("", actual, matcher);
     }
-    
+
     public static <T> void assertThat(String reason, T actual, Matcher<? super T> matcher) {
-        if (!matcher.matches(actual)) {
-            Description description = new StringDescription();
-            description.appendText(reason)
-                       .appendText("\nExpected: ")
-                       .appendDescriptionOf(matcher)
-                       .appendText("\n     but: ");
-            matcher.describeMismatch(actual, description);
-            
-            throw new AssertionError(description.toString());
-        }
+        MatcherExecute.match(reason, actual, matcher, new AssertionErrorStrategy());
     }
-    
+
     public static void assertThat(String reason, boolean assertion) {
         if (!assertion) {
             throw new AssertionError(reason);
