91df5152ebd624308ab7fef46d821dc5744600a6
Steve Freeman <github@m3p.co.uk>
2014-05-28 21:29:44 +0100

    tidying tests

--- hamcrest-core/src/test/java/org/hamcrest/core/StringEndsWithTest.java
+++ hamcrest-core/src/test/java/org/hamcrest/core/StringEndsWithTest.java
@@ -10,27 +10,21 @@ import org.hamcrest.Matcher;
 
 public class StringEndsWithTest extends AbstractMatcherTest {
     static final String EXCERPT = "EXCERPT";
-    Matcher<String> stringEndsWith = endsWith(EXCERPT);
+    final Matcher<String> stringEndsWith = endsWith(EXCERPT);
 
     @Override
     protected Matcher<?> createMatcher() {
         return stringEndsWith;
     }
 
-    public void testEvaluatesToTrueIfArgumentContainsSpecifiedSubstring() {
-        assertFalse("should be false if excerpt at beginning",
-                stringEndsWith.matches(EXCERPT + "END"));
-        assertTrue("should be true if excerpt at end",
-                stringEndsWith.matches("START" + EXCERPT));
-        assertFalse("should be false if excerpt in middle",
-                stringEndsWith.matches("START" + EXCERPT + "END"));
-        assertTrue("should be true if excerpt is at end and repeated",
-                stringEndsWith.matches(EXCERPT + EXCERPT));
+    public void testMatchesSubstringAtEnd() {
+        assertMatches(stringEndsWith, EXCERPT + "END");
+        assertMatches(stringEndsWith, "START" + EXCERPT);
+        assertDoesNotMatch(stringEndsWith, "START" + EXCERPT + "END");
+        assertMatches(stringEndsWith, EXCERPT + EXCERPT);
 
-        assertFalse("should be false if excerpt is not in string",
-                stringEndsWith.matches("Something else"));
-        assertFalse("should be false if part of excerpt is at end of string",
-                stringEndsWith.matches(EXCERPT.substring(0, EXCERPT.length() - 2)));
+        assertDoesNotMatch(stringEndsWith, "EXCER");
+        assertMismatchDescription("bad", stringEndsWith, "Something else");
     }
 
     public void testEvaluatesToTrueIfArgumentIsEqualToSubstring() {
