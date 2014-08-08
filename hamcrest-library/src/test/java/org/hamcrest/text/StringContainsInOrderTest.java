5f1362ecc8969616187e197ab01eb59ad9a5136f
plow <simu@snowplow.ch>
2014-08-08 11:21:07 +0200

    Preventing StringContainsInOrder from matching multiple identical parameter strings with the same substring occurrence

--- hamcrest-library/src/test/java/org/hamcrest/text/StringContainsInOrderTest.java
+++ hamcrest-library/src/test/java/org/hamcrest/text/StringContainsInOrderTest.java
@@ -7,7 +7,7 @@ import org.hamcrest.Matcher;
 
 
 public class StringContainsInOrderTest extends AbstractMatcherTest {
-    StringContainsInOrder m = new StringContainsInOrder(asList("a", "b", "c"));
+    StringContainsInOrder m = new StringContainsInOrder(asList("a", "b", "c", "c"));
 
     @Override
     protected Matcher<?> createMatcher() {
@@ -15,16 +15,18 @@ public class StringContainsInOrderTest extends AbstractMatcherTest {
     }
     
     public void testMatchesOnlyIfStringContainsGivenSubstringsInTheSameOrder() {
-        assertMatches("substrings in order", m, "abc");
-        assertMatches("substrings separated", m, "1a2b3c4");
+        assertMatches("all substrings adjacent in order", m, "abcc");
+        assertMatches("all substrings separated in order", m, "1a2b3c4c5");
         
-        assertDoesNotMatch("substrings out of order", m, "cab");
-        assertDoesNotMatch("no substrings in string", m, "xyz");
-        assertDoesNotMatch("substring missing", m, "ac");
+        assertDoesNotMatch("cumulative substring length longer than string", m, "abc");
+        assertDoesNotMatch("substrings out of order", m, "cabc");
+        assertDoesNotMatch("no substrings in string", m, "wxyz");
+        assertDoesNotMatch("substring not occurring", m, "axcc");
+        assertDoesNotMatch("substring missing", m, "acc");
         assertDoesNotMatch("empty string", m, "");
     }
     
     public void testHasAReadableDescription() {
-        assertDescription("a string containing \"a\", \"b\", \"c\" in order", m);
+        assertDescription("a string containing \"a\", \"b\", \"c\", \"c\" in order", m);
     }
 }
