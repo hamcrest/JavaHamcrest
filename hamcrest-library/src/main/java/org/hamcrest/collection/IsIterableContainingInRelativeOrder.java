5ed2d06688f9d3b445b98b13056223b79318a614
Doug Lawrie <doug@neverfear.org>
2014-11-16 15:47:42 +0000

    Matcher that allows the user to specify a partial ordering (a relative order) between items in an iterable. The iterable may contain gaps inbetween two matching items. Gaps may appear at the beginning, end or in between. A user may specify may gaps in the sequence.
    e.g. Given an iterable 1, 2, 3, 4, 5, 6, 7 if the user were to specify
    containsInRelativeOrder(2, 4, 6) this would match successfully.
    
    This commit is the direct incorporation of a patch provided by Kevin Pauli.
    
    See also https://groups.google.com/forum/?fromgroups#!topic/hamcrest-dev/FsLlyRcGPzs

---
+++ hamcrest-library/src/main/java/org/hamcrest/collection/IsIterableContainingInRelativeOrder.java
@@ -0,0 +1,124 @@
+package org.hamcrest.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class IsIterableContainingInRelativeOrder<E> extends TypeSafeDiagnosingMatcher<Iterable<? extends E>> {
+    private final List<Matcher<? super E>> matchers;
+
+    public IsIterableContainingInRelativeOrder(List<Matcher<? super E>> matchers) {
+        this.matchers = matchers;
+    }
+
+    @Override
+    protected boolean matchesSafely(Iterable<? extends E> iterable, Description mismatchDescription) {
+        MatchSeriesInRelativeOrder<E> matchSeriesInRelativeOrder = new MatchSeriesInRelativeOrder<E>(matchers, mismatchDescription);
+        matchSeriesInRelativeOrder.processItems(iterable);
+        return matchSeriesInRelativeOrder.isFinished();
+    }
+
+    public void describeTo(Description description) {
+        description.appendText("iterable containing ").appendList("[", ", ", "]", matchers).appendText(" in relative order");
+    }
+
+    private static class MatchSeriesInRelativeOrder<F> {
+        public final List<Matcher<? super F>> matchers;
+        private final Description mismatchDescription;
+        private int nextMatchIx = 0;
+        private F lastMatchedItem = null;
+
+        public MatchSeriesInRelativeOrder(List<Matcher<? super F>> matchers, Description mismatchDescription) {
+            this.mismatchDescription = mismatchDescription;
+            if (matchers.isEmpty()) {
+                throw new IllegalArgumentException("Should specify at least one expected element");
+            }
+            this.matchers = matchers;
+        }
+
+        public void processItems(Iterable<? extends F> iterable) {
+            for (F item : iterable) {
+                if (nextMatchIx < matchers.size()) {
+                    Matcher<? super F> matcher = matchers.get(nextMatchIx);
+                    if (matcher.matches(item)) {
+                        lastMatchedItem = item;
+                        nextMatchIx++;
+                    }
+                }
+            }
+        }
+
+        public boolean isFinished() {
+            if (nextMatchIx < matchers.size()) {
+                mismatchDescription.appendDescriptionOf(matchers.get(nextMatchIx)).appendText(" was not found");
+                if (lastMatchedItem != null) {
+                    mismatchDescription.appendText(" after ").appendValue(lastMatchedItem);
+                }
+                return false;
+            }
+            return true;
+        }
+
+    }
+
+    /**
+     * Creates a matcher for {@link Iterable}s that matches when a single pass over the
+     * examined {@link Iterable} yields a series of items, that contains items logically equal to the
+     * corresponding item in the specified items, in the same relative order
+     * <p/>
+     * For example:
+     * <pre>assertThat(Arrays.asList("a", "b", "c", "d", "e"), containsInRelativeOrder("b", "d"))</pre>
+     * 
+     * @param items
+     *     the items that must be contained within items provided by an examined {@link Iterable} in the same relative order
+     */
+    @Factory
+    public static <E> Matcher<Iterable<? extends E>> containsInRelativeOrder(E... items) {
+        List<Matcher<? super E>> matchers = new ArrayList<Matcher<? super E>>();
+        for (E item : items) {
+            matchers.add(equalTo(item));
+        }
+
+        return containsInRelativeOrder(matchers);
+    }
+
+    /**
+     * Creates a matcher for {@link Iterable}s that matches when a single pass over the
+     * examined {@link Iterable} yields a series of items, that each satisfying the corresponding
+     * matcher in the specified matchers, in the same relative order.
+     * <p/>
+     * For example:
+     * <pre>assertThat(Arrays.asList("a", "b", "c", "d", "e"), containsInRelativeOrder(equalTo("b"), equalTo("d")))</pre>
+     * 
+     * @param itemMatchers
+     *     the matchers that must be satisfied by the items provided by an examined {@link Iterable} in the same relative order
+     */
+    @Factory
+    public static <E> Matcher<Iterable<? extends E>> containsInRelativeOrder(Matcher<? super E>... itemMatchers) {
+        return containsInRelativeOrder(asList(itemMatchers));
+    }
+
+    /**
+     * Creates a matcher for {@link Iterable}s that matches when a single pass over the
+     * examined {@link Iterable} yields a series of items, that contains items satisfying the corresponding
+     * matcher in the specified list of matchers, in the same relative order.
+     * <p/>
+     * For example:
+     * <pre>assertThat(Arrays.asList("a", "b", "c", "d", "e"), contains(Arrays.asList(equalTo("b"), equalTo("d"))))</pre>
+     * 
+     * @param itemMatchers
+     *     a list of matchers, each of which must be satisfied by the items provided by
+     *     an examined {@link Iterable} in the same relative order
+     */
+    @Factory
+    public static <E> Matcher<Iterable<? extends E>> containsInRelativeOrder(List<Matcher<? super E>> itemMatchers) {
+        return new IsIterableContainingInRelativeOrder<E>(itemMatchers);
+    }
+}
---
+++ hamcrest-library/src/test/java/org/hamcrest/collection/IsIterableContainingInRelativeOrderTest.java
@@ -0,0 +1,81 @@
+package org.hamcrest.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.AbstractMatcherTest;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.Matcher;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.collection.IsIterableContainingInRelativeOrder.containsInRelativeOrder;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+@SuppressWarnings("unchecked")
+public class IsIterableContainingInRelativeOrderTest extends AbstractMatcherTest {
+    // temporary hack until the Java type system works
+    private final Matcher<Iterable<? extends WithValue>> contains123 = containsInRelativeOrder(value(1), value(2), value(3));
+
+    @Override
+    protected Matcher<?> createMatcher() {
+        return containsInRelativeOrder(1, 2);
+    }
+
+    public void testMatchingSingleItemIterable() throws Exception {
+        assertMatches("Single item iterable", containsInRelativeOrder(1), asList(1));
+    }
+
+    public void testMatchingMultipleItemIterable() throws Exception {
+        assertMatches("Multiple item iterable", containsInRelativeOrder(1, 2, 3), asList(1, 2, 3));
+    }
+
+    public void testMatchesWithMoreElementsThanExpectedAtBeginning() throws Exception {
+        assertMatches("More elements at beginning", containsInRelativeOrder(2, 3, 4), asList(1, 2, 3, 4));
+    }
+
+    public void testMatchesWithMoreElementsThanExpectedAtEnd() throws Exception {
+        assertMatches("More elements at end", containsInRelativeOrder(1, 2, 3), asList(1, 2, 3, 4));
+    }
+
+    public void testMatchesWithMoreElementsThanExpectedInBetween() throws Exception {
+        assertMatches("More elements in between", containsInRelativeOrder(1, 3), asList(1, 2, 3));
+    }
+
+    public void testDoesNotMatchWithFewerElementsThanExpected() throws Exception {
+        List<WithValue> valueList = asList(make(1), make(2));
+        assertMismatchDescription("value with <3> was not found after <WithValue 2>", contains123, valueList);
+    }
+
+    public void testDoesNotMatchIfSingleItemNotFound() throws Exception {
+        assertMismatchDescription("value with <4> was not found", containsInRelativeOrder(value(4)), asList(make(3)));
+    }
+
+    public void testDoesNotMatchIfOneOfMultipleItemsNotFound() throws Exception {
+        assertMismatchDescription("value with <3> was not found after <WithValue 2>", contains123, asList(make(1), make(2), make(4)));
+    }
+
+    public void testDoesNotMatchEmptyIterable() throws Exception {
+        assertMismatchDescription("value with <4> was not found", containsInRelativeOrder(value(4)), new ArrayList<WithValue>());
+    }
+
+    public void testHasAReadableDescription() {
+        assertDescription("iterable containing [<1>, <2>] in relative order", containsInRelativeOrder(1, 2));
+    }
+
+    public static class WithValue {
+      private final int value;
+      public WithValue(int value) { this.value = value; }
+      public int getValue() { return value; }
+      @Override public String toString() { return "WithValue " + value; }
+    }
+
+    public static WithValue make(int value) {
+      return new WithValue(value);
+    }
+
+    public static Matcher<WithValue> value(int value) {
+      return new FeatureMatcher<WithValue, Integer>(equalTo(value), "value with", "value") {
+        @Override protected Integer featureValueOf(WithValue actual) { return actual.getValue(); }
+      };
+    }
+}
