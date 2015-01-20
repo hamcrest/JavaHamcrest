28cdb8bfbb0ec78994c2ba0a77173085888a0f71
sf105 <github@m3p.co.uk>
2015-01-20 14:18:41 +0000

    remove unused files

--- hamcrest-generator/src/main/java/org/hamcrest/generator/SugarConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.hamcrest.generator;
-
-public interface SugarConfiguration {
-    void addWriter(FactoryWriter factoryWriter);
-
-    void addFactoryMethod(FactoryMethod method);
-
-    void addFactoryMethods(Iterable<FactoryMethod> methods);
-}
