b87f7b85d5a353af86f7e517574f6e7e120c189b
Frieze, Ashley (ELS-OXF) <a.frieze@elsevier.com>
2015-05-13 21:43:52 +0100

    Added Javadoc and removed redundant public function

--- hamcrest-core/src/main/java/org/hamcrest/MatcherAssert.java
+++ hamcrest-core/src/main/java/org/hamcrest/MatcherAssert.java
@@ -1,14 +1,45 @@
 package org.hamcrest;
 
+/**
+ * Provides assertions for use in unit tests etc
+ */
 public class MatcherAssert {
+	/**
+	 * Assert that a value is matched by a matcher
+	 * @param actual actual value
+	 * @param matcher to match against the actual
+	 * @throws AssertionError if the matcher does not match with the actual
+	 */
     public static <T> void assertThat(T actual, Matcher<? super T> matcher) {
         assertThat("", actual, matcher);
     }
     
+	/**
+	 * Assert that a value is matched by a matcher, with a reason
+	 * explaining the meaning of the assertion
+	 * @param reason a description of the meaning of the assertion
+	 * @param actual actual value
+	 * @param matcher to match against the actual
+	 * @throws AssertionError if the matcher does not match with the actual
+	 */
     public static <T> void assertThat(String reason, T actual, Matcher<? super T> matcher) {
     	assertThat(selfDescribingString(reason), actual, matcher);
     }
     
+	/**
+	 * Assert that a value is matched by a matcher, with a describer
+	 * to explain the meaning of the assertion. The interface for the
+	 * reason it intended for providing a description just in time
+	 * rather than requiring the caller to calculate the description ahead of
+	 * it being needed in the assertion message. The aim is to allow
+	 * a lambda to be provided to describe the error if required, e.g.
+	 *     assertThat(d->d.appendText("Checking validity of ").appendText(something.toString(), 
+	 *         someValue, is(expectedValue));
+	 * @param reason an object that can describe the meaning of the assertion
+	 * @param actual actual value
+	 * @param matcher to match against the actual
+	 * @throws AssertionError if the matcher does not match with the actual
+	 */
     public static <T> void assertThat(SelfDescribing reason, T actual, Matcher<? super T> matcher) {
         if (!matcher.matches(actual)) {
             Description description = new StringDescription();
@@ -22,8 +53,28 @@ public class MatcherAssert {
         }
     }
     
+    /**
+     * Assert that a value is true
+     * @param reason description of what is being tested
+     * @param assertion a value that is expected to be true for
+     * @throws AssertionError if assertion is false
+     */
     public static void assertThat(String reason, boolean assertion) {
     	assertThat(selfDescribingString(reason), assertion);
+    }
+
+    /**
+     * Assert that a value is true
+     * @param reason an object that can describe the reason for the test
+     * @param assertion a value that is expected to be true
+     * @throws AssertionError if assertion is false
+     */
+	public static void assertThat(SelfDescribing reason, boolean assertion) {
+        if (!assertion) {
+        	Description description = new StringDescription();
+        	reason.describeTo(description);
+            throw new AssertionError(description.toString());
+        }
     }
     
     private static SelfDescribing selfDescribingString(final String reason) {
@@ -34,22 +85,5 @@ public class MatcherAssert {
 			}
 			
 		};
-	}
-
-	public static void assertThat(SelfDescribing reason, boolean assertion) {
-        if (!assertion) {
-        	Description description = new StringDescription();
-        	reason.describeTo(description);
-            throw new AssertionError(description.toString());
-        }
-    }
-    
-    public static SelfDescribing describeAsString(final String string) {
-    	return new SelfDescribing() {
-			@Override
-			public void describeTo(Description description) {
-				description.appendText(string);
-			}
-    	};
-    }
+	}    
 }
