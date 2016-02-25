e532388735aa8483f1692c76cbec7bd13cab0c67
Burak Bayrakdar <burak.bayrakdar@rakuten.com>
2016-02-25 11:28:02 +0900

    class constraint for java.lang.Boolean

--- hamcrest-library/src/main/java/org/hamcrest/beans/HasPropertyWithValue.java
+++ hamcrest-library/src/main/java/org/hamcrest/beans/HasPropertyWithValue.java
@@ -93,7 +93,7 @@ public class HasPropertyWithValue<T> extends TypeSafeDiagnosingMatcher<T> {
     private Condition<PropertyDescriptor> propertyOn(T bean, Description mismatch) {
         PropertyDescriptor property = PropertyUtil.getPropertyDescriptor(propertyName, bean);
         if (property != null) {
-            if (property.getReadMethod() == null) {
+            if (property.getReadMethod() == null && property.getPropertyType().equals(Boolean.class)) {
                 String booleanGetter = "is" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
                 for(Method method : bean.getClass().getDeclaredMethods()) {
                     if (method.getName().equals(booleanGetter)) {
