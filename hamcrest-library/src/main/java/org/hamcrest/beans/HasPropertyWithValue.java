41153483c1e69a3edc8cffa6ece681aff8b061bb
Burak Bayrakdar <burak.bayrakdar@rakuten.com>
2016-02-25 11:06:15 +0900

    refactor 4 same logic as b4

--- hamcrest-library/src/main/java/org/hamcrest/beans/HasPropertyWithValue.java
+++ hamcrest-library/src/main/java/org/hamcrest/beans/HasPropertyWithValue.java
@@ -92,14 +92,16 @@ public class HasPropertyWithValue<T> extends TypeSafeDiagnosingMatcher<T> {
 
     private Condition<PropertyDescriptor> propertyOn(T bean, Description mismatch) {
         PropertyDescriptor property = PropertyUtil.getPropertyDescriptor(propertyName, bean);
-        if(property != null && property.getReadMethod() == null) {
-            String booleanGetter = "is" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
-            for(Method method : bean.getClass().getDeclaredMethods()) {
-                if (method.getName().equals(booleanGetter)) {
-                    try {
-                        property.setReadMethod(method);
-                    } catch (IntrospectionException e) {
-                        throw new IllegalStateException("Cannot set read method" + e);
+        if (property != null) {
+            if (property.getReadMethod() == null) {
+                String booleanGetter = "is" + propertyName.substring(0, 1).toUpperCase() + propertyName.substring(1);
+                for(Method method : bean.getClass().getDeclaredMethods()) {
+                    if (method.getName().equals(booleanGetter)) {
+                        try {
+                            property.setReadMethod(method);
+                        } catch (IntrospectionException e) {
+                            throw new IllegalStateException("Cannot set read method" + e);
+                        }
                     }
                 }
             }
