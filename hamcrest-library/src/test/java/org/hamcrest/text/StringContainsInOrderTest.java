3ede8bf0d4626bac10e145cb4b1541c677b8bece
sf105 <github@m3p.co.uk>
2016-04-06 16:22:56 +0200

    added extra test

--- hamcrest-library/src/test/java/org/hamcrest/text/StringContainsInOrderTest.java
+++ hamcrest-library/src/test/java/org/hamcrest/text/StringContainsInOrderTest.java
@@ -4,28 +4,30 @@ import org.hamcrest.AbstractMatcherTest;
 import org.hamcrest.Matcher;
 
 import static java.util.Arrays.asList;
+import static org.hamcrest.text.StringContainsInOrder.stringContainsInOrder;
 
 
 public class StringContainsInOrderTest extends AbstractMatcherTest {
-    StringContainsInOrder m = new StringContainsInOrder(asList("a", "b", "c", "c"));
+    final StringContainsInOrder matcher = new StringContainsInOrder(asList("a", "b", "c", "c"));
 
     @Override
     protected Matcher<?> createMatcher() {
-        return m;
+        return matcher;
     }
     
     public void testMatchesOnlyIfStringContainsGivenSubstringsInTheSameOrder() {
-        assertMatches("substrings in order", m, "abcc");
-        assertMatches("substrings separated", m, "1a2b3c4c5");
-        
-        assertDoesNotMatch("substrings in order missing a repeated pattern", m, "abc");
-        assertDoesNotMatch("substrings out of order", m, "cab");
-        assertDoesNotMatch("no substrings in string", m, "xyz");
-        assertDoesNotMatch("substring missing", m, "ac");
-        assertDoesNotMatch("empty string", m, "");
+        assertMatches("substrings in order", matcher, "abcc");
+        assertMatches("substrings separated", matcher, "1a2b3c4c5");
+
+        assertDoesNotMatch("can detect repeated strings for matching", stringContainsInOrder("abc", "abc"), "---abc---");
+        assertDoesNotMatch("substrings in order missing a repeated pattern", matcher, "abc");
+        assertDoesNotMatch("substrings out of order", matcher, "cab");
+        assertDoesNotMatch("no substrings in string", matcher, "xyz");
+        assertDoesNotMatch("substring missing", matcher, "ac");
+        assertDoesNotMatch("empty string", matcher, "");
     }
     
     public void testHasAReadableDescription() {
-        assertDescription("a string containing \"a\", \"b\", \"c\", \"c\" in order", m);
+        assertDescription("a string containing \"a\", \"b\", \"c\", \"c\" in order", matcher);
     }
 }
