c445ec3ab2f685998f22bcb6e3a55da05fad6ea5
sf105 <github@m3p.co.uk>
2016-04-07 15:12:35 +0200

    Tidy up generics. Test to prove https://github.com/hamcrest/JavaHamcrest/pull/38

--- hamcrest-core/src/main/java/org/hamcrest/core/IsAnything.java
+++ hamcrest-core/src/main/java/org/hamcrest/core/IsAnything.java
@@ -33,9 +33,7 @@ public class IsAnything<T> extends BaseMatcher<T> {
     /**
      * Creates a matcher that always matches, regardless of the examined object.
      */
-    public static Matcher<Object> anything() {
-        return new IsAnything<Object>();
-    }
+    public static Matcher<Object> anything() { return new IsAnything<>(); }
 
     /**
      * Creates a matcher that always matches, regardless of the examined object, but describes
@@ -45,6 +43,6 @@ public class IsAnything<T> extends BaseMatcher<T> {
      *     a meaningful {@link String} used when describing itself
      */
     public static Matcher<Object> anything(String description) {
-        return new IsAnything<Object>(description);
+        return new IsAnything<>(description);
     }
 }
