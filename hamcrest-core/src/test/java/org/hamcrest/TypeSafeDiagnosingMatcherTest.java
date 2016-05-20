e1e1890dfade824dbb82eca90d2bcb8325856324
sf105 <github@m3p.co.uk>
2016-05-20 17:18:31 +0100

    TypeSafeDiagnosingMatcher can't detect generic types for subclass

--- hamcrest-core/src/test/java/org/hamcrest/TypeSafeDiagnosingMatcherTest.java
+++ hamcrest-core/src/test/java/org/hamcrest/TypeSafeDiagnosingMatcherTest.java
@@ -1,6 +1,5 @@
 package org.hamcrest;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.AbstractMatcherTest.*;
@@ -10,27 +9,15 @@ import static org.hamcrest.AbstractMatcherTest.*;
  */
 public class TypeSafeDiagnosingMatcherTest {
 
-  private final TypeSafeDiagnosingMatcher stringMatcher = new TypeSafeDiagnosingMatcher<String>() {
-    @Override
-    protected boolean matchesSafely(String item, Description mismatchDescription) {
-      mismatchDescription.appendText("mismatching");
-      return false;
+    @Test public void
+    describesMismatches() {
+        assertMismatchDescription("was null", STRING_MATCHER, null);
+        assertMismatchDescription("was Character \"c\"", STRING_MATCHER, 'c');
+        assertMismatchDescription("mismatching", STRING_MATCHER, "other");
     }
 
-    @Override
-    public void describeTo(Description description) { }
-  };
-
-
-  @Test public void
-  describesMismatches() {
-    assertMismatchDescription("was null", stringMatcher, null);
-    assertMismatchDescription("was Character \"c\"", stringMatcher, 'c');
-    assertMismatchDescription("mismatching", stringMatcher, "other");
-  }
-
-  @Test public void
-  detects_non_builtin_types() {
+    @Test public void
+    detects_non_builtin_types() {
       final Matcher<NotBuiltIn> matcher = new TypeSafeDiagnosingMatcher<NotBuiltIn>() {
         @Override
         protected boolean matchesSafely(NotBuiltIn item, Description mismatchDescription) {
@@ -42,30 +29,52 @@ public class TypeSafeDiagnosingMatcherTest {
 
       assertMatches("not built in", matcher, new NotBuiltIn());
       assertDoesNotMatch("other not built in", (Matcher)matcher, new OtherNotBuiltIn());
-  }
+    }
 
-  @Ignore("not yet") @Test public void
-  detects_for_subtypes_of_matcher() {
-      final Matcher<NotBuiltIn> matcher = new SubMatcher<>();
+    @Test public void
+    filters_type_for_subclassed_matcher_when_expected_type_passed_in() {
+      final Matcher<NotBuiltIn> matcher = new SubMatcher<>(new NotBuiltIn());
 
       assertMatches("not built in", matcher, new NotBuiltIn());
       assertDoesNotMatch("other not built in", (Matcher)matcher, new OtherNotBuiltIn());
   }
 
+    @Test public void
+    but_cannot_detect_generic_type_in_subclassed_matcher_using_reflection() {
+        final Matcher<NotBuiltIn> matcher = new SubMatcher<>();
 
-  public static class SubMatcher<T> extends TypeSafeDiagnosingMatcher<T> {
-    @Override protected boolean matchesSafely(T item, Description mismatchDescription) { return true; }
+        assertMatches("not built in", matcher, new NotBuiltIn());
+        assertMatches("other not built in", (Matcher)matcher, new OtherNotBuiltIn());
+    }
 
-    @Override public void describeTo(Description description) { description.appendText("sub type"); }
-  }
+    private static final TypeSafeDiagnosingMatcher STRING_MATCHER = new TypeSafeDiagnosingMatcher<String>() {
+        @Override
+        protected boolean matchesSafely(String item, Description mismatchDescription) {
+          mismatchDescription.appendText("mismatching");
+          return false;
+        }
 
-  public static class NotBuiltIn {
+        @Override
+        public void describeTo(Description description) { }
+    };
+
+    public static class SubMatcher<T> extends TypeSafeDiagnosingMatcher<T> {
+      public SubMatcher() {
+          super();
+      }
+      public SubMatcher(T expectedObject) {
+          super(expectedObject.getClass());
+      }
+      @Override protected boolean matchesSafely(T item, Description mismatchDescription) { return true; }
+      @Override public void describeTo(Description description) { description.appendText("sub type"); }
+    }
+
+    public static class NotBuiltIn {
       public final String value = "not built in";
       @Override public String toString() { return "NotBuiltIn"; }
-  }
+    }
 
-  public static class OtherNotBuiltIn {
-
-  }
+    public static class OtherNotBuiltIn { // empty
+    }
 
 }
