0da90880b3aa7364afba05b7f1a853a25c190a44
Joe Schmetzer <joe@exubero.com>
2018-11-29 18:22:12 +0000

    Build empty jars for hamcrest-core and -library. (#236)
    Existing maven builds that depend upon either `hamcrest-core-1.3.jar`
    or `hamcrest-library.jar` will not upgrade their version for a pom-only
    artifact. This change re-introduces a jar file for -core and -library,
    but with no real classes. This allows Maven builds to correctly detect
    the latest version of the jar. The transitive dependency on `hamcrest.jar`
    brings in the latest version of the classes that were previously in
    both `hamcrest-core.jar` or `hamcrest-library.jar`.

---
+++ hamcrest-core/src/main/java/org/hamcrest/HamcrestCoreIsDeprecated.java
@@ -0,0 +1,9 @@
+package org.hamcrest;
+
+/**
+ * All the classes in <code>hamcrest-core.jar</code> have moved to
+ * <code>hamcrest.jar</code>. Please use that dependency instead.
+ */
+@Deprecated
+class HamcrestCoreIsDeprecated {
+}
---
+++ hamcrest-core/src/main/java/org/hamcrest/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * All classes in <code>hamcrest-core.jar</code> have been migrated to
+ * <code>hamcrest.jar</code>. Please use that dependency instead.
+ */
+package org.hamcrest;
