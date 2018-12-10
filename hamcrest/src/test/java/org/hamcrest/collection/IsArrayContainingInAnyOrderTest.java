faac95ae402b1819ce6bca5c9c0b8afd78d65f4f
Joe Schmetzer <joe@exubero.com>
2018-12-10 22:06:50 +0000

    Fix failing tests.

--- hamcrest/src/test/java/org/hamcrest/collection/IsArrayContainingInAnyOrderTest.java
+++ hamcrest/src/test/java/org/hamcrest/collection/IsArrayContainingInAnyOrderTest.java
@@ -36,8 +36,8 @@ public class IsArrayContainingInAnyOrderTest extends AbstractMatcherTest {
     public void testMismatchesItemsInAnyOrder() {
       Matcher<Integer[]> matcher = arrayContainingInAnyOrder(1, 2, 3);
       assertMismatchDescription("was null", matcher, null);
-      assertMismatchDescription("No item matches: <1>, <2>, <3> in []", matcher, new Integer[] {});
-      assertMismatchDescription("No item matches: <2>, <3> in [<1>]", matcher, new Integer[] {1});
-      assertMismatchDescription("Not matched: <4>", matcher, new Integer[] {4,3,2,1});
+      assertMismatchDescription("no item matches: <1>, <2>, <3> in []", matcher, new Integer[] {});
+      assertMismatchDescription("no item matches: <2>, <3> in [<1>]", matcher, new Integer[] {1});
+      assertMismatchDescription("not matched: <4>", matcher, new Integer[] {4,3,2,1});
     }
 }
