6118614d2177aa2d9ea772a7d78aa41d03b45fd1
Marat Bukharov <maratik@yandex-team.ru>
2021-09-09 17:06:38 +0300

    Add ignorable fields to HasEqualValues

--- hamcrest/src/main/java/org/hamcrest/object/HasEqualValues.java
+++ hamcrest/src/main/java/org/hamcrest/object/HasEqualValues.java
@@ -7,10 +7,10 @@ import org.hamcrest.TypeSafeDiagnosingMatcher;
 import org.hamcrest.core.IsEqual;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
 
 public class HasEqualValues<T> extends TypeSafeDiagnosingMatcher<T> {
 
@@ -18,9 +18,13 @@ public class HasEqualValues<T> extends TypeSafeDiagnosingMatcher<T> {
     private final List<FieldMatcher> fieldMatchers;
 
     public HasEqualValues(T expectedObject) {
+        this(expectedObject, Collections.<String>emptySet());
+    }
+
+    public HasEqualValues(T expectedObject, Set<String> ignored) {
         super(expectedObject.getClass());
         this.expectedObject = expectedObject;
-        this.fieldMatchers = fieldMatchers(expectedObject);
+        this.fieldMatchers = fieldMatchers(expectedObject, ignored);
     }
 
     @Override
@@ -67,10 +71,12 @@ public class HasEqualValues<T> extends TypeSafeDiagnosingMatcher<T> {
         }
     }
 
-    private static List<FieldMatcher> fieldMatchers(Object expectedObject) {
+    private static List<FieldMatcher> fieldMatchers(Object expectedObject, Set<String> ignored) {
         final List<FieldMatcher> result = new ArrayList<>();
         for (Field field : expectedObject.getClass().getFields()) {
-            result.add(new FieldMatcher(field, expectedObject));
+            if (!ignored.contains(field.getName())) {
+                result.add(new FieldMatcher(field, expectedObject));
+            }
         }
         return result;
     }
@@ -83,4 +89,21 @@ public class HasEqualValues<T> extends TypeSafeDiagnosingMatcher<T> {
         }
     }
 
+    /**
+     * Creates a matcher that matches when the examined object has values for all of
+     * its fields that are equal to the corresponding values of the
+     * specified object. If any fields are marked as ignored, they will be dropped from
+     * both the expected and actual object.
+     * For example:
+     * <pre>assertThat(myObject, hasEqualValues(myExpectedObject))</pre>
+     * <pre>assertThat(myObject, hasEqualValues(myExpectedObject), "age", "height")</pre>
+     *
+     * @param expectedObject
+     *     the object against which examined objects are compared
+     * @param ignoredFields
+     *     do not check any of these named fields.
+     */
+    public static <B> Matcher<B> hasEqualValues(B expectedObject, String... ignoredFields) {
+        return new HasEqualValues<>(expectedObject, new HashSet<>(asList(ignoredFields)));
+    }
 }
--- hamcrest/src/test/java/org/hamcrest/object/HasEqualsValuesTest.java
+++ hamcrest/src/test/java/org/hamcrest/object/HasEqualsValuesTest.java
@@ -3,11 +3,13 @@ package org.hamcrest.object;
 import org.hamcrest.AbstractMatcherTest;
 import org.hamcrest.Matcher;
 
+import java.util.Collections;
+
 @SuppressWarnings("WeakerAccess")
 public class HasEqualsValuesTest extends AbstractMatcherTest {
 
-    private static final WithPublicFields WITH_PUBLIC_FIELDS = new WithPublicFields('x', 666, "a string");
-    private static final HasEqualValues<WithPublicFields> WITH_PUBLIC_FIELDS_MATCHER = new HasEqualValues<>(WITH_PUBLIC_FIELDS);
+    private static final WithPublicFields WITH_PUBLIC_FIELDS = new WithPublicFields('x', 666, "a string", false);
+    private static final HasEqualValues<WithPublicFields> WITH_PUBLIC_FIELDS_MATCHER = new HasEqualValues<>(WITH_PUBLIC_FIELDS, Collections.singleton("ignorable"));
 
     @Override
     protected Matcher<?> createMatcher() {
@@ -21,29 +23,31 @@ public class HasEqualsValuesTest extends AbstractMatcherTest {
     }
 
     public void test_matches_equivalent_object() {
-        assertMatches(WITH_PUBLIC_FIELDS_MATCHER, new WithPublicFields('x', 666, "a string"));
+        assertMatches(WITH_PUBLIC_FIELDS_MATCHER, new WithPublicFields('x', 666, "a string", true));
     }
 
     public void test_mismatches_on_first_field_inequality() {
         assertMismatchDescription(
                 "'s' was \"different\"",
-                WITH_PUBLIC_FIELDS_MATCHER, new WithPublicFields('x', 666, "different"));
+                WITH_PUBLIC_FIELDS_MATCHER, new WithPublicFields('x', 666, "different", true));
     }
 
     public void test_mismatches_on_inherited_field() {
         assertMismatchDescription(
                 "'c' was \"y\"",
-                WITH_PUBLIC_FIELDS_MATCHER, new WithPublicFields('y', 666, "a string"));
+                WITH_PUBLIC_FIELDS_MATCHER, new WithPublicFields('y', 666, "a string", true));
     }
 
     public static class WithPublicFields extends Parent {
         public final int i;
         public final String s;
+        public final boolean ignorable;
 
-        public WithPublicFields(char c, int i, String s) {
+        public WithPublicFields(char c, int i, String s, boolean ignorable) {
             super(c);
             this.i = i;
             this.s = s;
+            this.ignorable = ignorable;
         }
     }
 
