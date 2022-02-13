6378ae1f9720af8ac9d164320ad92427b5cc8967
John Patrick <142304+nhojpatrick@users.noreply.github.com>
2022-02-13 20:58:33 +0000

    Java Code Style trailing whitespace org.hamcrest.internal

--- hamcrest/src/main/java/org/hamcrest/internal/ArrayIterator.java
+++ hamcrest/src/main/java/org/hamcrest/internal/ArrayIterator.java
@@ -6,14 +6,14 @@ import java.util.Iterator;
 public class ArrayIterator implements Iterator<Object> {
     private final Object array;
     private int currentIndex = 0;
-    
+
     public ArrayIterator(Object array) {
         if (!array.getClass().isArray()) {
             throw new IllegalArgumentException("not an array");
         }
         this.array = array;
     }
-    
+
     @Override
     public boolean hasNext() {
         return currentIndex < Array.getLength(array);
@@ -23,7 +23,7 @@ public class ArrayIterator implements Iterator<Object> {
     public Object next() {
         return Array.get(array, currentIndex++);
     }
-    
+
     @Override
     public void remove() {
         throw new UnsupportedOperationException("cannot remove items from an array");
--- hamcrest/src/main/java/org/hamcrest/internal/ReflectiveTypeFinder.java
+++ hamcrest/src/main/java/org/hamcrest/internal/ReflectiveTypeFinder.java
@@ -1,25 +1,25 @@
 /**
- * The TypeSafe classes, and their descendants, need a mechanism to find out what type has been used as a parameter 
- * for the concrete matcher. Unfortunately, this type is lost during type erasure so we need to use reflection 
- * to get it back, by picking out the type of a known parameter to a known method. 
- * The catch is that, with bridging methods, this type is only visible in the class that actually implements 
+ * The TypeSafe classes, and their descendants, need a mechanism to find out what type has been used as a parameter
+ * for the concrete matcher. Unfortunately, this type is lost during type erasure so we need to use reflection
+ * to get it back, by picking out the type of a known parameter to a known method.
+ * The catch is that, with bridging methods, this type is only visible in the class that actually implements
  * the expected method, so the ReflectiveTypeFinder needs to be applied to that class or a subtype.
- * 
+ *
  * For example, the abstract <code>TypeSafeDiagnosingMatcher&lt;T&gt;</code> defines an abstract method
  * <pre>protected abstract boolean matchesSafely(T item, Description mismatchDescription);</pre>
  * By default it uses <code>new ReflectiveTypeFinder("matchesSafely", 2, 0); </code> to find the
  * parameterised type. If we create a <code>TypeSafeDiagnosingMatcher&lt;String&gt;</code>, the type
  * finder will return <code>String.class</code>.
- * 
- * A <code>FeatureMatcher</code> is an abstract subclass of <code>TypeSafeDiagnosingMatcher</code>. 
+ *
+ * A <code>FeatureMatcher</code> is an abstract subclass of <code>TypeSafeDiagnosingMatcher</code>.
  * Although it has a templated implementation of <code>matchesSafely(&lt;T&gt;, Description);</code>, the
  * actual run-time signature of this is <code>matchesSafely(Object, Description);</code>. Instead,
- * we must find the type by reflecting on the concrete implementation of 
+ * we must find the type by reflecting on the concrete implementation of
  * <pre>protected abstract U featureValueOf(T actual);</pre>
  * a method which is declared in <code>FeatureMatcher</code>.
- * 
- * In short, use this to extract a type from a method in the leaf class of a templated class hierarchy. 
- *  
+ *
+ * In short, use this to extract a type from a method in the leaf class of a templated class hierarchy.
+ *
  * @author Steve Freeman
  * @author Nat Pryce
  */
@@ -37,7 +37,7 @@ public class ReflectiveTypeFinder {
     this.expectedNumberOfParameters = expectedNumberOfParameters;
     this.typedParameter = typedParameter;
   }
-  
+
   public Class<?> findExpectedType(Class<?> fromClass) {
     for (Class<?> c = fromClass; c != Object.class; c = c.getSuperclass()) {
         for (Method method : c.getDeclaredMethods()) {
