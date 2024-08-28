3019f1bee22f9666f89ce286a1a1facc3c8eba7f
Sergey Chernov <serega.morph@gmail.com>
2024-08-29 00:32:10 +0200

    Optional matchers (#421)

---
+++ hamcrest/src/main/java/org/hamcrest/optional/OptionalEmpty.java
@@ -0,0 +1,36 @@
+package org.hamcrest.optional;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+
+import java.util.Optional;
+
+/**
+ * Matcher that expects empty {@link Optional}.
+
+ * @param <T> type of {@link Optional} value
+ */
+public class OptionalEmpty<T> extends TypeSafeDiagnosingMatcher<Optional<T>> {
+
+    /**
+     * Matcher that expects empty {@link Optional}.
+     *
+     * @param <T> type of optional value
+     * @return The matcher.
+     */
+    public static <T> Matcher<Optional<T>> emptyOptional() {
+        return new OptionalEmpty<>();
+    }
+
+    @Override
+    protected boolean matchesSafely(Optional<T> value, Description mismatchDescription) {
+        mismatchDescription.appendText("is " + value);
+        return !value.isPresent();
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("empty");
+    }
+}
