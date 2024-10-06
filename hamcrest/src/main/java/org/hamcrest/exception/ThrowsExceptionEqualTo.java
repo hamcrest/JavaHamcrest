94e0c1e7cb64f953951ea563a1894b0407259e79
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-10-06 22:54:14 +0200

    Change: Port Throwable matchers to `TypeSafeDiagnosingMatcher`

--- hamcrest/src/main/java/org/hamcrest/exception/ThrowsExceptionEqualTo.java
+++ hamcrest/src/main/java/org/hamcrest/exception/ThrowsExceptionEqualTo.java
@@ -1,14 +1,14 @@
 package org.hamcrest.exception;
 
 import org.hamcrest.Description;
-import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
 
 /**
  * A matcher that checks if a Runnable throws an expected exception when run.
  *
  * @param <T> the type of the expected exception.
  */
-public class ThrowsExceptionEqualTo<T extends Throwable> extends TypeSafeMatcher<T> {
+public class ThrowsExceptionEqualTo<T extends Throwable> extends TypeSafeDiagnosingMatcher<T> {
     private final T expectedException;
 
     /**
@@ -25,21 +25,21 @@ public class ThrowsExceptionEqualTo<T extends Throwable> extends TypeSafeMatcher
     }
 
     @Override
-    public boolean matchesSafely(T item) {
-        return this.expectedException.getClass().isAssignableFrom(item.getClass()) && item.getMessage().equals(this.expectedException.getMessage());
+    public boolean matchesSafely(T item, Description mismatchDescription) {
+        if (!this.expectedException.getClass().isAssignableFrom(item.getClass())) {
+            mismatchDescription.appendValue(item.getClass().getName()).appendText(" instead of a ").appendValue(this.expectedException.getClass().getName()).appendText(" exception");
+            return false;
+        }
+        if (!item.getMessage().equals(this.expectedException.getMessage())) {
+            mismatchDescription.appendValue(item.getClass().getName()).appendText(" with message ").appendValue(item.getMessage()).appendText(" instead of ").appendValue(this.expectedException.getMessage());
+            return false;
+        }
+
+        return true;
     }
 
     @Override
     public void describeTo(Description description) {
         description.appendValue(expectedException.getClass().getName()).appendText(" with message ").appendValue(expectedException.getMessage());
-    }
-
-    @Override
-    protected void describeMismatchSafely(T item, Description mismatchDescription) {
-        if (this.expectedException.getClass().isAssignableFrom(item.getClass())) {
-            mismatchDescription.appendValue(item.getClass().getName()).appendText(" with message ").appendValue(item.getMessage()).appendText(" instead of ").appendValue(this.expectedException.getMessage());
-            return;
-        }
-        mismatchDescription.appendValue(item.getClass().getName()).appendText(" instead of a ").appendValue(this.expectedException.getClass().getName()).appendText(" exception");
     }
 }
